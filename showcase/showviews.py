93f0474dfd6d7963f1d6d125f9ae6017831fba7a
linjt <47111326@qq.com>
2019-03-26 20:17:52 +0800

    new

---
+++ showcase/showviews.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.shortcuts import render
+
+
+# Create your views here.
+def base(request):
+    return render(request, 'showcase/base.html')
