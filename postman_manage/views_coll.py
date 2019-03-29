ba249ec83a659a9973e84d1fff58c68834199f3c
John Doe <john@sample.com>
2019-03-29 15:55:04 +0800

    实现同时获取多个用户的collections

--- postman_manage/views_coll.py
+++ postman_manage/views_coll.py
@@ -1,9 +1,14 @@
+import django
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Api_Manage.settings'
+django.setup()
 import requests
 import json
 import pymysql
 import datetime
-from postman_manage.models import Collections
 from django.shortcuts import render
+from postman_manage.models import Collections
+
 
 con = pymysql.connect(
     host="127.0.0.1",
@@ -32,27 +37,27 @@ def write_db(sql):
 
 
 def get_collection(request):
-    url = "https://api.getpostman.com/collections"
-    headers = {"X-Api-Key": "a0b4bb86e8f246fdb49212b75e2a8da1"}
-    # headers = {"X-Api-Key": "a0b4bb86e8f246fdb49212b75e2a8da1"}
-    collections = requests.get(url, headers=headers).json()['collections']
+    select_xkey = 'select xkey from postman_manage_xkey'
+    xkeys = read_db(select_xkey)
+    print(xkeys)
     delete_collections = 'delete from postman_manage_collections'
     write_db(delete_collections)
-    for i in range(len(collections)):
-        collection_id = collections[i]['id']
-        collection_name = collections[i]['name']
-        collection_owner = collections[i]['owner']
-        collection_uid = collections[i]['uid']
-        values = (collection_id, collection_name, collection_owner, collection_uid)
-        inster_collections = 'INSERT INTO postman_manage_collections(collection_id,collection_name,collection_owner,collection_uid) values' + str(
-            values)
-        write_db(inster_collections)
+    for a in range(len(xkeys)):
+        xkey = xkeys[a]['xkey']
+        url = "https://api.getpostman.com/collections"
+        headers = {"X-Api-Key": xkey}
+        # headers = {"X-Api-Key": "a0b4bb86e8f246fdb49212b75e2a8da1"}
+        collections = requests.get(url, headers=headers).json()['collections']
+        for i in range(len(collections)):
+            collection_id = collections[i]['id']
+            collection_name = collections[i]['name']
+            collection_owner = collections[i]['owner']
+            collection_uid = collections[i]['uid']
+            values = (collection_id, collection_name, collection_owner, collection_uid)
+            inster_collections = 'INSERT INTO postman_manage_collections(collection_id,collection_name,collection_owner,collection_uid) values' + str(
+                values)
+            write_db(inster_collections)
 
     username = request.session.get('user', '')  # 读取浏览器登录session
     collection_list = Collections.objects.all()  # 读取collection
     return render(request, "collections_manage.html", {"user": username, "collections": collection_list})
-
-# sql = "select xkey from postman_manage_xkey"
-# xkeys = read_db(sql)
-#
-# print(xkeys)
