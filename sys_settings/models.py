c951665273c4ae03e838eb5acdbc2ca5fb4e9568
John Doe <john@sample.com>
2019-04-16 09:32:56 +0800

    新增数据库管理功能

---
+++ sys_settings/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Dbs(models.Model):
+    name = models.CharField('name', max_length=200)  # 数据库名称
+    host = models.CharField('host', max_length=200)  # host
+    port = models.CharField('port', max_length=200)  # port
+    user = models.CharField('user', max_length=200)  # user
+    password = models.CharField('password', max_length=200)  # password
+    db = models.CharField('db', max_length=200)  # db
+    create_time = models.DateTimeField('创建时间', auto_now=True)  # 创建时间，自动获取当前时间
+
+    class Meta:
+        verbose_name = 'Dbs'
+        verbose_name_plural = 'Dbs'
+
+    def __str__(self):
+        return self.Dbs
